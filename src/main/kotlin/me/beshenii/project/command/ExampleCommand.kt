d1c539aba755cbaa0e98c65836e6d0a94458fe86
BesheniiChitak <artemkvash83@mail.ru>
2024-07-19 16:07:30 +0500

    коммит

--- src/main/kotlin/me/beshenii/project/command/ExampleCommand.kt
+++ src/main/kotlin/me/beshenii/project/command/ExampleCommand.kt
@@ -3,12 +3,22 @@ package me.beshenii.project.command
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
 
-object ExampleCommand : CommandExecutor {
+object ExampleCommand : CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, command: Command, s: String, args: Array<String>): Boolean {
         if (args.size != 1) return false
         sender.sendMessage("1")
         return true
+    }
+
+    override fun onTabComplete(
+        p0: CommandSender,
+        p1: Command,
+        p2: String,
+        p3: Array<out String>?
+    ): MutableList<String> {
+        return mutableListOf()
     }
 }
 
